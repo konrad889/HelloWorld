af095f84b0eb022c7741b0635c581776ffc19e79
Konrad Tomczyk <konrad.tomczyk@windowslive.com>
2014-10-28 14:47:15 +0100

    test gita

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Hello = "Hello World";
+            Console.WriteLine(Hello);
+            Console.ReadKey();
+        }
+    }
+}
